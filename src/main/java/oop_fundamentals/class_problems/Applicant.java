fcbd51c5f4585e9d1545794ee9045c6dd58bd977
Joshua Thomas Joseph <joshuathomasjoseph@192.168.1.2>
2026-09-26 17:26:10 +0530

    Completed session 3 questions

---
+++ src/main/java/oop_fundamentals/class_problems/Applicant.java
@@ -0,0 +1,17 @@
+package main.java.oop_fundamentals.class_problems;
+
+public class Applicant {
+    static int totalApplicants = 0;
+
+    public Applicant() {
+        totalApplicants++;
+    }
+
+    public static void main(String[] args) {
+        Applicant a1 = new Applicant();
+        Applicant a2 = new Applicant();
+        Applicant a3 = new Applicant();
+
+        System.out.println("Total applicants: " + Applicant.totalApplicants);
+    }
+}
---
+++ src/main/java/oop_fundamentals/class_problems/Course.java
@@ -0,0 +1,17 @@
+package main.java.oop_fundamentals.class_problems;
+
+public class Course {
+    String code;
+    String title;
+
+    public Course(String code, String title) {
+        this.code = code;
+        this.title = title;
+    }
+
+    public static void main(String[] args) {
+        Course course = new Course("21CSC201J", "Data Structures");
+
+        System.out.println(course.code + " - " + course.title);
+    }
+}
